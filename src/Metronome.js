3c6ce2bc3add129cc9bfbd7334cdcda3881e3e9f
Grayson Gayle <grayson.gayle@utexas.edu>
2019-01-04 11:06:03 -0600

    init

---
+++ src/Metronome.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from "react";
+import Slider from "@material-ui/lab/Slider";
+import { Typography } from "@material-ui/core";
+export class Metronome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bpm: 50
+        };
+    }
+    moveSlider = (event, value) => {
+        this.setState({ bpm: value });
+    };
+
+    render() {
+        return (
+            <div>
+             
+                <div>
+
+                    <Typography id="slider-image">Metronome {this.state.bpm}</Typography>
+                    <Slider
+                        style={{ padding: "22px 22px", color: "blue", width: 300 }}
+                        aria-labelledby="slider-image"
+                        min={40}
+                        max={220}
+                        step={1}
+                        value={this.state.bpm}
+                        onChange={this.moveSlider} />
+                        
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { AppBar, Toolbar, createMuiTheme, MuiThemeProvider, Paper, Drawer, ListItem, ListItemText, Divider } from "@material-ui/core"
+import { Metronome } from "./Metronome";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#353535",
+      main: "#0E0E0E",
+      dark: "#0E0E0E",
+      contrastText: "white",
+    }
+  },
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      met: 50
+    }
+  }
+
+
+
+  render() {
+    return <MuiThemeProvider theme={theme}>
+      <div className="App">
+
+        <AppBar className="AppBar" position="static">
+          <Toolbar>
+            Practice Routine
+
+          </Toolbar>
+
+        </AppBar>
+        Slider
+        <Metronome/>
+
+        {/* <Drawer  anchor="left" open variant="persistent">
+     
+          <ListItem style={{marginTop: 64}} button>
+            <ListItemText primary="First" />
+            <Divider />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="Second" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="Third" />
+          </ListItem>
+          <Divider />
+        </Drawer> */}
+        {/* <iframe src="../assets/Vacchiano-Trumpet-Routines.pdf" type="application/pdf" height="900px" width="500px" /> */}
+      </div>
+    </MuiThemeProvider>
+  }
+}
+
+const drawerWidth = 240;
+const styles = {
+  drawer: {
+    width: drawerWidth
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("index"));
